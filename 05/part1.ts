74168cab5221e148ed90fdd902352dc2c9b1b94b
James Monger <jameskmonger@hotmail.co.uk>
2020-12-07 18:09:56 +0000

    Day 5 part 1

---
+++ 05/part1.ts
@@ -0,0 +1,58 @@
+import { input } from "./input";
+
+enum BSPInstruction { UPPER, LOWER }
+
+// assumes there are no invalid characters in the input
+const generateBSPInstructions =
+    (lowerChar: string, input: string): BSPInstruction[] =>
+        input.split("").map(c => c === lowerChar ? BSPInstruction.LOWER : BSPInstruction.UPPER);
+
+const BOARDING_PASS_REGEX = /((?:B|F){7})((?:L|R){3})/;
+const splitBoardingPassInstructions = (pass: string): { row: BSPInstruction[], col: BSPInstruction[] } => {
+    const result = BOARDING_PASS_REGEX.exec(pass);
+
+    if (result === null) {
+        return null;
+    }
+
+    return {
+        row: generateBSPInstructions("F", result[1]),
+        col: generateBSPInstructions("L", result[2])
+    };
+};
+
+const calculateBSPLocation = (instructions: BSPInstruction[], max: number, min: number = 0): number => {
+    const next = instructions.shift();
+
+    const midPoint = Math.ceil((max - min) / 2);
+
+    if (instructions.length === 0) {
+        if (next === BSPInstruction.LOWER) {
+            return min;
+        } else if (next === BSPInstruction.UPPER) {
+            return max;
+        }
+    }
+
+    if (next === BSPInstruction.LOWER) {
+        max = max - midPoint;
+    } else if (next === BSPInstruction.UPPER) {
+        min = min + midPoint;
+    }
+
+    return calculateBSPLocation(instructions, max, min);
+};
+
+const calculateSeatId = ({ row, col }: { row: BSPInstruction[], col: BSPInstruction[] }) => {
+    const rowPosition = calculateBSPLocation(row, 127);
+    const colPosition = calculateBSPLocation(col, 7);
+
+    return rowPosition * 8 + colPosition;
+};
+
+const seatIds = input.split("\n").map(splitBoardingPassInstructions).map(calculateSeatId);
+seatIds.sort((a, b) => b - a);
+
+const answer = seatIds[0];
+
+console.log(answer);
